cb7d007bbb256bc87278dc869c11ebeff3c00a62
Jonathan Amiez <jonathan.amiez@gmail.com>
2013-02-08 11:41:07 +0100

    rework data loading to avoid recreating instances

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -14,8 +14,6 @@ class Provider < ActiveRecord::Base
   has_many :instances, :inverse_of => :provider
 
   def load_metrics(data)
-    Instance.destroy_all
-    Instance.reset_pk_sequence
     new_entities = []
     new_metrics = []
 
@@ -23,26 +21,35 @@ class Provider < ActiveRecord::Base
       # first, entity
       # retrieve DB row if any, or create one
       entity = Entity.find_or_create_by_name(entity_name)
+      new_entities << entity.id
+
+      instances = Hash[Instance.includes(:metric).where(:entity_id => entity.id).map {|i| [i.metric.id, i]}]
 
       # then metrics and instances
       metrics.each do |metric_h|
-        m = metric_type.new(metric_h)
+        metric_found, instance_found = false, false
 
-        instance = Instance.new
-        instance.assign_attributes({:entity => entity, :provider => self }, :without_protection => true)
-        instance.details = m.instance_details unless m.instance_details.nil?
+        m = metric_type.new(metric_h)
+        instance_details = m.details.merge(m.instance_details)
 
         # retrieve DB row if any, or create one
         if metric = metric_type.find_by_name(m.name)
+          metric_found = true
           metric.update_attributes!(:details => m.details)
-        else
-          metric = m
-          metric.save!
+          if instances.has_key?(metric.id)
+            instance_found = true
+            instances[metric.id].update_attributes!(:details => instance_details)
+          end
+        end
+
+        unless instance_found
+          unless metric_found
+            metric = m
+            metric.save!
+          end
+          Instance.create(:entity_id => entity.id, :metric_id => metric.id, :provider_id => self.id, :details => instance_details)
         end
         new_metrics << metric.id
-
-        instance.metric = metric
-        instance.save!
       end
     end
 
