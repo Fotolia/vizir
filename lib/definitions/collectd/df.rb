1fa1323dabeee4ab001e1872a57446e4e66ba685
Renaud Chaput <renchap@cocoa-x.com>
2013-02-11 10:25:37 +0100

    Dont override df metrics with df_complex ones

--- lib/definitions/collectd/df.rb
+++ lib/definitions/collectd/df.rb
@@ -1,15 +1,15 @@
-# Simple DF
-[ "used", "free" ].each do |x|
+types = %w(used reserved free)
+
+types.each do |x|
+  # Simple DF
   metric "df_#{x}" do
     rrd %r{df/df-(?<mount>\S+).rrd}
     ds x
     title "Disk space #{x} on $mount"
   end
-end
 
-# Complex DF
-%w(used free reserved).each do |type|
-  metric "df_#{type}" do
+  # Complex DF
+  metric "df_complex_#{type}" do
     rrd %r{df-(?<mount>\S+)/df_complex-#{type}.rrd}
     ds "value"
     title "Disk space #{type} on $mount"
@@ -17,7 +17,7 @@ end
 end
 
 graph "df" do
-  metrics [ "df_used", "df_reserved", "df_free" ]
+  metrics types.map {|t| "df_complex_#{t}"} + types.map {|t| "df_#{t}"}
   layout :area
   title "Disk space on $mount"
   scope "mount"
