75ba981c5e5bfe48fe7765be44c100ac220e2ade
Renaud Chaput <renchap@cocoa-x.com>
2013-02-13 20:56:12 +0100

    Fix the metrics sorting in menu

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -2,7 +2,7 @@ class EntitiesController < ApplicationController
   def menu_metrics
     e = Entity.find(params[:id].to_i)
     if e
-      @graphs = e.graphs.includes(:instances)
+      @graphs = e.graphs.includes(:instances).sort_by { |g| g.title }
     end
     render :layout => false
   end
