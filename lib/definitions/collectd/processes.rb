9b9edcedd1cf779ca5c822c0534bf52e44e910bf
Jonathan Amiez <jonathan.amiez@gmail.com>
2013-02-20 12:28:30 +0100

    add system dashboard

--- lib/definitions/collectd/processes.rb
+++ lib/definitions/collectd/processes.rb
@@ -1,13 +1,15 @@
-graph "processes" do
-  %w(zombies wait blocked stopped running sleeping idle).each do |type|
-    metric "processes_#{type}" do
-      rrd %r{processes/ps_state-#{type}.rrd}
-      ds "value"
-      title "#{type.capitalize} processes"
+dashboard "system" do
+  graph "processes" do
+    %w(zombies wait blocked stopped running sleeping idle).each do |type|
+      metric "processes_#{type}" do
+        rrd %r{processes/ps_state-#{type}.rrd}
+          ds "value"
+        title "#{type.capitalize} processes"
+      end
     end
-  end
 
-  layout :area
-  title "Processes"
-  scope :entity
+    layout :area
+    title "Processes"
+    scope :entity
+  end
 end
