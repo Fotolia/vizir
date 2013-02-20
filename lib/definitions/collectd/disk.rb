b96024275b2b467c08910e807434d6f15ef7dfb8
Jonathan Amiez <jonathan.amiez@gmail.com>
2013-02-20 13:49:00 +0100

    add more dashboards

--- lib/definitions/collectd/disk.rb
+++ lib/definitions/collectd/disk.rb
@@ -1,13 +1,17 @@
-graph "disk_octets" do
-  [ "read", "write" ].each do |type|
-    metric "disk_octets_#{type}" do
-      rrd %r{disk-(?<disk>[\w\-]+)/disk_octets.rrd}
-      ds type
-      title "Disk $disk octets #{type}"
+dashboard "disks" do
+  graph "disk_octets" do
+    [ "read", "write" ].each do |type|
+      metric "disk_octets_#{type}" do
+        rrd %r{disk-(?<disk>[\w\-]+)/disk_octets.rrd}
+        ds type
+        title "Disk $disk octets #{type}"
+      end
     end
+
+    layout :line
+    title "Disk $disk I/O"
+    scope "disk"
   end
 
-  layout :line
-  title "Disk $disk I/O"
-  scope "disk"
+  title "Disks"
 end
